8df7ccdba9e47fdb4a7ad36235bdf2ce8bc8120c
Sean Sundberg <seansund@us.ibm.com>
2021-01-22 18:17:21 -0600

    Outputs groups

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,14 @@ output "ids" {
   description = "List of ids of generated resource groups"
   value       = data.ibm_resource_group.resource_group.*.id
 }
+
+output "groups" {
+  description = "List of group names and ids"
+  value       = [
+    for index in data.ibm_resource_group.resource_group:
+      {
+        id   = data.ibm_resource_group.resource_group[index].id
+        name = data.ibm_resource_group.resource_group[index].name
+      }
+  ]
+}
