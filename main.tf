d7d193ef9f5479af84d0d0134d6eea681d094c75
Sean Sundberg <seansund@us.ibm.com>
2021-04-12 16:01:07 -0500

    Adds timeout values to create and destroy namespace
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -3,6 +3,11 @@ resource ibm_resource_group resource_group {
   count = var.provision ? 1 : 0
 
   name  = var.resource_group_name
+
+  timeouts {
+    create  = "15m"
+    destroy = "15m"
+  }
 }
 
 data ibm_resource_group resource_group {
