65a1756058fdb3f9006ca4b3af69bce37318d2c1
Sean Sundberg <seansund@us.ibm.com>
2021-04-16 02:10:38 -0500

    Adds ibmcloud_api_key as explicit variable
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ variable "resource_group_name" {
   description = "The name of the resource group"
 }
 
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "The IBM Cloud api token"
+}
+
 variable "provision" {
   type        = bool
   description = "Flag indicating that the resource group should be created"
