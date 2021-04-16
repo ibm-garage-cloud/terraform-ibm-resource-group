ac1dbcee3055f54c5e069cb974f04fdc6bb6051e
Sean Sundberg <seansund@us.ibm.com>
2021-04-16 02:12:04 -0500

    Adds ibmcloud_api_key to test variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -4,3 +4,8 @@ variable "new_resource_group" {
   type        = string
   description = "Resource group that will be created"
 }
+
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "IBM Cloud API Key"
+}
